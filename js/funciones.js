ea672922042dc406a0f127a2f5ddcf8bf4bffa5c
unknown <pipebkn_999@hotmail.com>
2018-04-24 20:54:06 -0300

    cambio 2

--- js/funciones.js
+++ js/funciones.js
@@ -3,26 +3,19 @@ var lat = '-33.4349083';
 var lon = '-70.6170132';
 document.addEventListener("deviceready", onDeviceReady, false);
 
-
-function showAlert(msj)
-{
-    navigator.notification.alert(
-        msj,  // message
-        'UNAB',   // title
-        ''    // buttonName
-    );
+function showAlert(msj) {
+  navigator.notification.alert(msj,  // message
+  'UNAB',   // title
+  ''    // buttonName
+  );
 }//fin function mensaje.
 
 // PhoneGap is ready
-    function onDeviceReady() 
-    {
-		// Do cool things here...
-      //getPosition();
-      if (! SMS ) { alert( 'SMS plugin not ready' ); return; }
-      
-    }
- 
-
+function onDeviceReady() {
+  // Do cool things here...
+  //getPosition();
+  if (! SMS ) { alert( 'SMS plugin not ready' ); return; }
+}
 
 function sendSMS()
 {
@@ -42,7 +35,7 @@ function sendSMS()
     var textoURI = mensajeSMS;
 	 if(SMS) 
 	 {
-	   SMS.sendSMS(fono,mensajeSMS, "Esto es una prueba de SMS con Cordova/Phonegap.."+textoURl, function () { showAlert('Message sent successfully');}, function (e) { showAlert('Message Failed:' + e);});
+	   SMS.sendSMS(fono,mensajeSMS, function () { showAlert('Message sent successfully');}, function (e) { showAlert('Message Failed:' + e);});
 	 }
 	  
   }
